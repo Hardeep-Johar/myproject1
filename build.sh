7a60c90c3c950488b7cc7e0b03c0b7586c89acfb
Hardeep-Johar <hardeep.johar@gmail.com>
2023-01-09 10:30:25 -0500

    fix build

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 pip install -r requirements.txt
-sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
-sudo apt-get update
-sudo apt-get install gdal-bin
-sudo apt-get install libgdal-dev
+add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+apt-get update
+apt-get install gdal-bin
+apt-get install libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 pip install GDAL
